30329b90bfb01a81b60b3391e5413ee2e71c64f9
Herculano Silva <85914689+herculanosilva@users.noreply.github.com>
2023-10-26 17:59:18 -0300

    Policy
    Criação da RolePolicy e PermissionPolicy

--- app/Models/Permission.php
+++ app/Models/Permission.php
@@ -5,8 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Spatie\Permission\Models\Permission as ModelsPermission;
+use Spatie\Permission\Traits\HasRoles;
 
 class Permission extends ModelsPermission
 {
-    use HasFactory;
+    use HasFactory, HasRoles;
 }
--- app/Models/Role.php
+++ app/Models/Role.php
@@ -5,8 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Spatie\Permission\Models\Role as ModelsRole;
+use Spatie\Permission\Traits\HasRoles;
 
 class Role extends ModelsRole
 {
-    use HasFactory;
+    use HasFactory, HasRoles;
 }
